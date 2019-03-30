26b6d7615f3373ebd61d842bc0cea53223fe209c
Jovêncio, R <r.jovencio@avanade.com>
2019-03-30 09:25:07 -0300

    nossa primeira api

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-"teste"
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.status(200).send({message: 'Olá mundo esse é meu novo repositorio clonado.'})
+})
+
+app.listen(4001, ()=>{
+    console.log('Api inicializada na porta 4001');
+})
